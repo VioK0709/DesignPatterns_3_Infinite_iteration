d0b7e8261f8994153b87a2ea80fc6a3d09ef59ce
Violetta Korolkova <la_bella.vita@mail.ru>
2023-03-18 15:59:19 +0200

    Initial commit

---
+++ src/Randoms.java
@@ -0,0 +1,28 @@
+import java.util.Iterator;
+import java.util.PrimitiveIterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+
+    protected PrimitiveIterator.OfInt random;
+
+    public Randoms(int min, int max) {
+        random = new Random().ints(min, max + 1).iterator();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt();
+            }
+        };
+    }
+}
