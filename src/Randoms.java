2b737bd0143916764d9de4c2613adc2c325cc42b
Violetta Korolkova <la_bella.vita@mail.ru>
2023-03-18 19:21:09 +0200

    Changes

--- src/Randoms.java
+++ src/Randoms.java
@@ -1,27 +1,37 @@
 import java.util.Iterator;
-import java.util.PrimitiveIterator;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
 
-    protected PrimitiveIterator.OfInt random;
+    protected Random random;
+    protected int min, max;
 
     public Randoms(int min, int max) {
-        random = new Random().ints(min, max + 1).iterator();
+        this.random = new Random();
+        this.min = min;
+        this.max = max;
     }
 
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<>() {
+            int index = 0;
 
             @Override
             public boolean hasNext() {
-                return true;
+                if (index < max) {
+                    return true;
+                }
+                return false;
             }
 
             @Override
             public Integer next() {
-                return random.nextInt();
+                int diff = max - min;
+                if (index < max && diff >= 0 && diff != Integer.MAX_VALUE) {
+                    return random.nextInt(diff + 1) + min;
+                }
+                return null;
             }
         };
     }
