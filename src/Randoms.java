7fe78e049b402e961e36b1b89557385f1e2edace
Violetta Korolkova <la_bella.vita@mail.ru>
2023-03-19 10:43:45 +0200

    Changes

--- src/Randoms.java
+++ src/Randoms.java
@@ -15,7 +15,6 @@ public class Randoms implements Iterable<Integer> {
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<>() {
-            int index = 0;
 
             @Override
             public boolean hasNext() {
@@ -25,8 +24,7 @@ public class Randoms implements Iterable<Integer> {
             @Override
             public Integer next() {
                 int diff = max - min;
-                if (index < max && diff >= 0 && diff != Integer.MAX_VALUE) {
-                    index++;
+                if (diff >= 0 && diff != Integer.MAX_VALUE) {
                     return random.nextInt(diff + 1) + min;
                 }
                 return null;
